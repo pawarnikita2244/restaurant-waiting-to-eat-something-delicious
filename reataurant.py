453abd47f55f6e7e340147e2e944c91dc311d1d8
Nikita Pawar <168295446+pawarnikita2244@users.noreply.github.com>
2024-07-12 20:28:09 -0700

    Add files via upload

---
+++ reataurant.py
@@ -0,0 +1,41 @@
+#you are at a restaurant waiting to eat something delicious 😋 
+
+# #but the restaurant is too crowded 😕
+
+# you being smart 😎 want to figure out the time it would take before its your turn
+# the restaurant offers 5 products:
+# 🍕 pizza - 15 mins 
+# 🍔 burger - 10 mins
+# 🍝 pasta - 10 mins
+# 🍩 donut - 5 mins
+# 🍟 fries - 5 mins
+# for simplicity, assume the restaurant has a policy that one person can only order one item.
+
+# you have a friend working at the restaurant 👨‍🍳who helps you in getting the input on no. of people and their orders:
+print("1>pizza--------15min")
+print("2>Burger-------10min")
+print("3>pasta--------10min")
+print("4>donut--------5min")
+print("5>fries--------5min")
+
+menu_list=['pizza','burger','pasta','donut','fries']
+item_time=[15,10,10,5,5]
+
+order=[]
+no_of_customer=input("enter the number of customer=")
+order_item=input("enter the number of item=")
+order.append(order_item)
+print(order)
+
+
+menu_index=menu_list.index('pizza')
+print(no_of_customer)
+#print(item_time)
+for j in item_time:
+    print(j)
+for i in item_time:
+    print(i)
+    item_time.index()
+    total=total+i
+    print(total)
+    
